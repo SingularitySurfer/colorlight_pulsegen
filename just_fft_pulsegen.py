ec3c3cb77006d1112dfd8f02476956631142e693
SingularitySurfer <norman_krackow@gmx.de>
2020-10-19 12:56:17 +0200

    fft and interpolator working but not whishbone controlled yet

--- just_fft_pulsegen.py
+++ just_fft_pulsegen.py
@@ -6,7 +6,7 @@ from migen import *
 from misoc.interconnect.stream import Endpoint
 from litex.soc.interconnect.csr import *
 
-from super_interpolator import SuperInterpolator
+from super_interpolator import SuperCicUS
 from fft_generator_migen import Fft
 
 
@@ -15,7 +15,7 @@ class Pulsegen(Module, AutoCSR):
      Pulsegen for Colorlight
     """
 
-    def __init__(self, width_d=16, size_fft=128, r_max=4096):
+    def __init__(self, width_d=16, size_fft=256, r_max=4096):
 
         self.out0 = Signal((width_d, False))  # dac output0
 
@@ -23,39 +23,36 @@ class Pulsegen(Module, AutoCSR):
 
         self.go = CSRStorage(1, description="start pulsegen")
         pos = Signal(int(np.log2(size_fft)))
-        slow = Signal(2)
+        slow = Signal(10)
         go = Signal()
 
+        # assume fft initialized
         self.submodules.fft = fft = Fft(size_fft, True, width_d, width_d, width_d, 16, False)
-
-        self.submodules.inter = inter = SuperInterpolator(width_d, r_max)
 
         self.comb += [
             fft.start.eq(go),
+            #fft.scaling.eq(0xff),
             fft.en.eq(1),
-            fft.scaling.eq(0xff),
             fft.x_out_adr.eq(pos),
-            inter.input.data.eq(fft.x_out),
-            inter.r.eq(200),
             #self.out0.eq(pos<<7),
             #self.out0.eq(Cat(~fft.x_out[8], fft.x_out[:7], 0x00)),
-            self.out0.eq(Cat(inter.output.data0[:15], ~inter.output.data0[15])),
+            self.out0.eq(Cat(fft.x_out[16:16+7], ~fft.x_out[16+7]) << 8),
         ]
 
         self.sync += [
             slow.eq(slow + 1),
-            fft.x_in.eq((2 ** (width_d - 3)) - 1000),
+            fft.x_in.eq((2 ** (width_d - 4)) - 2000),
             fft.x_in_adr.eq(1),
             fft.x_in_we.eq(0),
             If((slow[-1] & ~go), go.eq(1), fft.x_in_we.eq(1)),
-            If(inter.input.ack & fft.done,
+            If(fft.done & (slow == 0),
                pos.eq(pos+1),
-               #If(pos<70, pos.eq(70)),
+               #If(pos<70, pos.eq(70))3
                #pos.eq(int(np.log2(size_fft))-1),
                )
         ]
     def sim(self):
-        for i in range(2000):
+        for i in range(500):
             yield
             #x = yield self.out0
             #if x > 700: print(x)
---
+++ pulsegen_main.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
+
+import os
+import argparse
+import sys
+
+from migen import *
+from migen.genlib.misc import WaitTimer
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex_boards.platforms import colorlight_5a_75b
+
+from litex.soc.cores.clock import *
+from litex.soc.cores.spi_flash import ECP5SPIFlash
+from litex.soc.cores.gpio import GPIOOut
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+
+from liteeth.phy.ecp5rgmii import LiteEthPHYRGMII
+from litex.build.generic_platform import *
+from litex.boards.platforms import genesys2
+
+from litex.soc.interconnect.csr import *
+
+from pulsegen import Pulsegen
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_gpios = [
+    ("gpio", 0, Pins("j4:0"), IOStandard("LVCMOS33")),
+    ("gpio", 1, Pins("j4:1"), IOStandard("LVCMOS33")),
+]
+
+# CRG ----------------------------------------------------------------------------------------------
+
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys    = ClockDomain()
+        # # #
+
+        # Clk / Rst
+        clk25 = platform.request("clk25")
+        rst_n = platform.request("user_btn_n", 0)
+        platform.add_period_constraint(clk25, 1e9/25e6)
+
+        # PLL
+        self.submodules.pll = pll = ECP5PLL()
+        self.comb += pll.reset.eq(~rst_n)
+        pll.register_clkin(clk25, 25e6)
+        pll.create_clkout(self.cd_sys, sys_clk_freq)
+
+
+class DAC(Module, AutoCSR):
+    """Basic first order sigma-delta DAC running at sys clock"""
+    def __init__(self):
+        self.inp = Signal((16))
+        self.out = Signal()
+        ###
+        accu = Signal(17)
+        self.sync += [
+            accu.eq(accu[:-1] + self.inp),
+            self.out.eq(accu[-1])
+        ]
+
+
+
+class Main(SoCMini, AutoCSR):
+    def __init__(self, with_etherbone=True, ip_address=None, mac_address=None):
+        platform     = colorlight_5a_75b.Platform(revision="7.0")
+        sys_clk_freq = int(60e6)
+
+        # SoCMini ----------------------------------------------------------------------------------
+        SoCMini.__init__(self, platform, clk_freq=sys_clk_freq)
+
+        # CRG --------------------------------------------------------------------------------------
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        # # Etherbone --------------------------------------------------------------------------------
+        # if with_etherbone:
+        #     self.submodules.ethphy = LiteEthPHYRGMII(
+        #         clock_pads = self.platform.request("eth_clocks"),
+        #         pads       = self.platform.request("eth"))
+        #     self.add_csr("ethphy")
+        #     self.add_etherbone(
+        #         phy         = self.ethphy,
+        #         ip_address  = ip_address,
+        #         mac_address = mac_address,
+        #     )
+        #
+        # # SPIFlash ---------------------------------------------------------------------------------
+        # self.submodules.spiflash = ECP5SPIFlash(
+        #     pads         = platform.request("spiflash"),
+        #     sys_clk_freq = sys_clk_freq,
+        #     spi_clk_freq = 5e6,
+        # )
+        # self.add_csr("spiflash")
+
+        # GPIOs ------------------------------------------------------------------------------------
+        platform.add_extension(_gpios)
+
+
+        # Led --------------------------------------------------------------------------------------
+        #self.submodules.led = GPIOOut(platform.request("user_led_n"))
+        #self.add_csr("led")
+
+        # Pulsegen RAM -----------------------------------------------------------------------------
+        # self.add_ram("pgen_ram", 0x10000000, 1024)
+        # port = self.pgen_ram.mem.get_port()
+        # self.specials += port
+
+        # PULSEGEN ---------------------------------------------------------------------------------
+        self.submodules.pulsegen = pg = Pulsegen(width_d=16, size_fft=128, r_max=4096)
+        self.add_csr("pulsegen")
+
+        # DAC --------------------------------------------------------------------------------------
+        self.submodules.dac = dac = DAC()
+        outp = platform.request("gpio", 1)
+        led = platform.request("user_led_n")
+        self.comb += [
+            dac.inp.eq(pg.out0),
+            outp.eq(self.dac.out),
+            led.eq(pg.go.storage),
+        ]
+
+
+
+# Build -------------------------------------------------------------------------------------------
+
+def main():
+    parser = argparse.ArgumentParser(description="pulsegen test using LiteX and colorlite")
+    parser.add_argument("--build",       action="store_true",      help="build bitstream")
+    parser.add_argument("--load",        action="store_true",      help="load bitstream")
+    parser.add_argument("--flash",       action="store_true",      help="flash bitstream")
+    parser.add_argument("--ip-address",  default="192.168.1.20",   help="Ethernet IP address of the board.")
+    parser.add_argument("--mac-address", default="0x726b895bc2e2", help="Ethernet MAC address of the board.")
+    args = parser.parse_args()
+
+    soc     = Main(ip_address=args.ip_address, mac_address=int(args.mac_address, 0))
+    builder = Builder(soc, output_dir="build", csr_csv="csr.csv")
+    builder.build(build_name="pulsegen", run=args.build)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".svf"))
+
+    if args.flash:
+        prog = soc.platform.create_programmer()
+        os.system("cp bit_to_flash.py build/gateware/")
+        os.system("cd build/gateware && ./bit_to_flash.py pulsegen.bit pulsegen.svf.flash")
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".svf.flash"))
+
+if __name__ == "__main__":
+    main()

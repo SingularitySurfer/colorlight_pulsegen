1e321f7140bf207ea9d911ff46051cd568cd0cda
SingularitySurfer <norman_krackow@gmx.de>
2020-10-21 16:06:02 +0200

    pulses in pulsegen script :)

--- scripts/pulsegen_script.py
+++ scripts/pulsegen_script.py
@@ -16,9 +16,9 @@ r = 200  # interpolation rate
 width_d = 16  # data width
 
 data = [0] * n
-data[1] = 16000 + 16000j
-data[3] = -16000 - 16000j
-data[9] = 8000 -8000j
+data[7] = 16000
+data[6] = -8000
+data[8] = -8000
 #data[8] = 8000
 
 data = [(int(_.real) | int(_.imag) << width_d) & (1 << width_d*2)-1 for _ in data]
